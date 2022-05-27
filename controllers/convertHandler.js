f380ebea7eff2b1c33ffdeb344fcbab36ec8c3b2
Melody Davis <piano.fan122@gmail.com>
2022-05-27 13:46:27 -0400

    began logic for initial numbers and units

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,63 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
-  };
+	this.getNum = function (input) {
+		let result = input;
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
-  };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
+		return result;
+	};
+
+	this.getUnit = function (input) {
+		let result;
+		switch (input) {
+			case 'gal':
+				result = 'gallons';
+				return result;
+			case 'kg':
+				result = 'kilograms';
+				return result;
+			case 'km':
+				result = 'kilometers';
+				return result;
+			case 'L':
+				result = 'liters';
+				return result;
+			case 'lb':
+			case 'lbs':
+				result = 'pounds';
+				return result;
+			case 'mi':
+				result = 'miles';
+				return result;
+			default:
+				return result;
+		}
+	};
+
+	this.getReturnUnit = function (initUnit) {
+		let result;
+
+		return result;
+	};
+
+	this.spellOutUnit = function (unit) {
+		let result;
+
+		return result;
+	};
+
+	this.convert = function (initNum, initUnit) {
+		const galToL = 3.78541;
+		const lbsToKg = 0.453592;
+		const miToKm = 1.60934;
+		let result;
+
+		return result;
+	};
+
+	this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+		let result;
+
+		return result;
+	};
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,29 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+	let convertHandler = new ConvertHandler();
+	app.get('/api/convert/', (req, res) => {
+		try {
+			const input = req.query.input;
 
+			let [inputNum, inputUnit] = input.split(/(.*\d)(\w+)/).slice(1, -1);
+
+			// make sure all units from input is lowercase except liters
+			inputUnit = inputUnit.toLowerCase();
+
+			if (inputUnit === 'l') {
+				inputUnit = 'L';
+			}
+
+			const num = convertHandler.getNum(inputNum);
+			const unit = convertHandler.getUnit(inputUnit);
+
+			const result = `${num} ${unit}`;
+			res
+				.status(200)
+				.json({ initNum: num, initUnit: inputUnit, string: result });
+		} catch (error) {
+			console.log(error);
+		}
+	});
 };
